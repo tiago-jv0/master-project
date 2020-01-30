821ed59b2d2c72fb8b08d76614c91ad638fb2af9
Tiago José <tiago_jvo@outlook.com>
2020-01-30 14:09:01 -0300

    created cartItem component and connected our cart dropdown with cartItems reducer

--- src/components/cartDropdown/CartDropdown.js
+++ src/components/cartDropdown/CartDropdown.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import './CartDropdown.scss'
+import React from 'react';
+import './CartDropdown.scss';
 
-import CustomButton from '../customButton/CustomButton'
+import CustomButton from '../customButton/CustomButton';
+import CartItem from '../cartItem/CartItem';
 
+import { connect } from 'react-redux';
 
-const CartDropdown = ({}) => {
+const CartDropdown = ({ cartItems }) => {
     return (
-        <div className='cart-dropdown'>
+        <div className="cart-dropdown">
             <div className="cart-items">
-
+                {cartItems.map((cartItem) => (
+                    <CartItem key={cartItem.id} item={cartItem}></CartItem>
+                ))}
             </div>
             <CustomButton> GO TO CHECKOUT</CustomButton>
         </div>
-    )
-}
+    );
+};
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
 
-export default CartDropdown
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cartItem/CartItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CartItem.scss';
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+    console.log(quantity);
+    return (
+        <div className="cart-item">
+            <img src={imageUrl} alt="item" srcset=" " />
+            <div className="item-details">
+                <span className="name">{name}</span>
+                <span className="price">
+                    {quantity} * ${price}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default CartItem;
